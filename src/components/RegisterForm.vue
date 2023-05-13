fa743d5d8abcea09a1de0d19a3b8b893b718f3ba
Daniel <awaritefe@hotmail.co.uk>
2023-05-13 20:02:04 +0100

    adding user file and keeping user logged in on page refresh

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -78,7 +78,20 @@
         <option value="Germany">Germany</option>
         <option value="Narnia">Narnia</option>
       </vee-field>
-      <Error-message class="text-red-600" name="country" />
+      <Error-message class="text-red-600" name="user_type" />
+    </div>
+    <!-- User type -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">User Type</label>
+      <vee-field
+        as="select"
+        name="user_type"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+      >
+        <option value="Listener">Listener</option>
+        <option value="Artist">Artist</option>
+      </vee-field>
+      <Error-message class="text-red-600" name="user_type" />
     </div>
     <!-- TOS -->
     <div class="mb-3 pl-6">
@@ -102,7 +115,8 @@
 </template>
 
 <script>
-import { auth, usersCollection } from '@/includes/firebase'
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'RegisterForm',
@@ -115,10 +129,12 @@ export default {
         password: 'required|min:9|max:20|not_one_of:password',
         confirm_password: 'passwords_mismatch:@password',
         country: 'required|country_excluded:Narnia',
+        user_type: 'required',
         tos: 'tos'
       },
       userData: {
-        country: 'UK'
+        country: 'UK',
+        user_type: 'Listener'
       },
       reg_in_submission: false,
       reg_show_alert: false,
@@ -127,29 +143,17 @@ export default {
     }
   },
   methods: {
+    ...mapActions(useUserStore, {
+      createUser: 'register'
+    }),
     async register(values) {
       this.reg_show_alert = true
       this.reg_in_submission = true
       this.reg_alert_variant = 'bg-blue-500'
       this.reg_alert_msg = 'Please wait, your account is being created!'
 
-      let userCred = null
       try {
-        userCred = await auth.createUserWithEmailAndPassword(values.email, values.password)
-      } catch (error) {
-        this.reg_in_submission = false
-        this.reg_alert_variant = 'bg-red-500'
-        this.reg_alert_msg = 'An unexpected error has occured. Please try again later.'
-        return
-      }
-
-      try {
-        await usersCollection.add({
-          name: values.name,
-          email: values.email,
-          age: values.age,
-          country: values.country
-        })
+        await this.createUser(values)
       } catch (error) {
         this.reg_in_submission = false
         this.reg_alert_variant = 'bg-red-500'
@@ -159,7 +163,6 @@ export default {
 
       this.reg_alert_variant = 'bg-green-500'
       this.reg_alert_msg = 'Success! Your account has been created.'
-      console.log(userCred)
     }
   }
 }
