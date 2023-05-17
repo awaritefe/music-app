1ed9c7bcc2c823a1a529a31129899a708a100d75
Daniel <awaritefe@hotmail.co.uk>
2023-05-17 16:28:42 +0100

    configuring routing and setting up routes for home and about

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -40,6 +40,9 @@
 </template>
 
 <script>
+import { mapActions } from 'pinia'
+import useUserStore from '@/stores/user'
+
 export default {
   name: 'LoginForm',
   data() {
@@ -55,14 +58,26 @@ export default {
     }
   },
   methods: {
-    login(values) {
-      this.login_in_submission = true
+    ...mapActions(useUserStore, ['authenticate']),
+    async login(values) {
       this.login_show_alert = true
+      this.login_in_submission = true
       this.login_alert_variant = 'bg-blue-500'
       this.login_alert_msg = 'Please wait, We are loging you in!'
 
+      try {
+        await this.authenticate(values)
+      } catch (error) {
+        this.login_in_submission = false
+        this.login_alert_variant = 'bg-red-500'
+        this.login_alert_msg = 'Invalid login details!'
+        return
+      }
+
       this.login_alert_variant = 'bg-green-500'
       this.login_alert_msg = 'Success! Your are now logged in.'
+
+      window.location.reload()
     }
   }
 }
