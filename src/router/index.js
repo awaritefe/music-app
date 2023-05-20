f761c1fe1ce761d7eb351493f9263ed20414fc1e
Daniel <awaritefe@hotmail.co.uk>
2023-05-20 12:18:45 +0100

    new route and view added for manage, active classes added and running through tailwind

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
+import Manage from '@/views/Manage.vue'
 
 const routes = [
   {
+    name: 'home',
     path: '/',
     component: HomeView
   },
   {
+    name: 'about',
     path: '/about',
     component: AboutView
+  },
+  {
+    name: 'manage',
+    path: '/manage',
+    component: Manage
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
