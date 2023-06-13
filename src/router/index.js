74eefe7e03213c7c8f96453a90008d23ea90a9a8
Daniel <awaritefe@hotmail.co.uk>
2023-06-13 14:03:37 +0100

    added page and links to single song with id

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import AboutView from '@/views/AboutView.vue'
 import Manage from '@/views/Manage.vue'
+import Song from '@/views/Song.vue'
 import useUserStore from '@/stores/user'
 
 const routes = [
@@ -41,6 +42,11 @@ const routes = [
   {
     path: '/manage',
     redirect: { name: 'manage' }
+  },
+  {
+    name: 'song',
+    path: '/song/:id',
+    component: Song
   },
   {
     path: '/:catchAll(.*)*',
