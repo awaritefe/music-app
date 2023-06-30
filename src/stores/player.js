186863b965aa5868bfdb796186553b59cf6dc735
Daniel <awaritefe@hotmail.co.uk>
2023-06-30 17:06:51 +0100

    formatting time on audio bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -1,15 +1,21 @@
 import { defineStore } from 'pinia'
 import { Howl } from 'howler'
+import helper from '@/includes/helper'
 
 export default defineStore('player', {
   state: () => ({
     current_song: {},
     sound: {},
     seek: '00:00',
-    duration: '00:00'
+    duration: '00:00',
+    playerProgress: '0%'
   }),
   actions: {
     async newSong(song) {
+      if (this.sound instanceof Howl) {
+        this.sound.unload()
+      }
+
       this.current_song = song
 
       this.sound = new Howl({
@@ -35,8 +41,10 @@ export default defineStore('player', {
       }
     },
     progress() {
-      this.seek = this.sound.seek()
-      this.duration = this.sound.duration()
+      this.seek = helper.formatTime(this.sound.seek())
+      this.duration = helper.formatTime(this.sound.duration())
+
+      this.playerProgress = `${(this.sound.seek() / this.sound.duration()) * 100}%`
 
       if (this.sound.playing()) {
         requestAnimationFrame(this.progress)
